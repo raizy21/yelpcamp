9a78ddd005f3b5e8abd47e11eb38193cce732dbf
Razvan Andrei <chiper_raizy@yahoo.com>
2024-12-24 10:25:43 +0100

    make data a js file and make a index js for the data

---
+++ seeds/index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  const c = new Campground({ title: "purlple field" });
+  await c.save();
+};
+
+seedDB();
